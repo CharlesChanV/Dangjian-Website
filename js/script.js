811c1ed27d1baf58e0015263552ed76b76b6c514
CharlesChanV <46051097+CharlesChanV@users.noreply.github.com>
2020-05-28 23:20:09 +0800

    init
    init

---
+++ js/script.js
@@ -0,0 +1,141 @@
+(function($) {
+    'use strict'
+
+    $(document).ready(function() {
+        
+
+        /*
+        ========================================
+        Top Nav
+        ========================================
+        */
+        $(window).on('scroll', function() {
+            var wScroll = $(this).scrollTop();
+            if (wScroll > 1) {
+                $('.top-nav').addClass('topnav');
+            } else {
+                $('.top-nav').removeClass('topnav');
+            };
+        });
+
+        /*
+        ========================================
+        doest work video
+        ========================================
+        */
+        $('.video-play-button').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+
+            fixedContentPos: false
+        });
+
+        /*
+        ========================================
+        Animet Aos
+        ========================================
+        */
+        AOS.init({
+            duration: 1000, // values from 0 to 3000, with step 50ms
+        });
+
+        /*
+        ========================================
+        Counter
+        ========================================
+        */
+        $('.counter').countUp({
+            'time': 3000,
+            'delay': 10
+        });
+
+        /*
+        ========================================
+        Testiminial Logo
+        ========================================
+        */
+        $('.testiminial-logo').owlCarousel({
+            loop: true,
+            navText: false,
+            dots: false,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 2
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
+                    items: 4
+                }
+            }
+        });
+
+        /*
+        ========================================
+        Testiminial Content
+        ========================================
+        */
+        $('.testiminial-content').owlCarousel({
+            loop: true,
+            navText: false,
+            dots: false,
+            autoplay: true,
+            autoplayTimeout: 4000,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2,
+                    margin: 30,
+                },
+                1000: {
+                    items: 3,
+                    margin: 30,
+                }
+            }
+        });
+
+        /*
+        ========================================
+        Footer Auto Height
+        ========================================
+        */
+        var footerHeight = $(".side-fixed").height();
+        $(".side-wrapper").css('margin-bottom', footerHeight + 'px');
+
+      
+    });
+
+
+    /*
+========================================
+Back 2 Top Bar
+========================================
+*/
+    const btn = $('#scroll2top');
+    const qrc = $('#miniprogram');
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+            qrc.addClass('show')
+        }else {
+            btn.removeClass('show');
+            qrc.removeClass('show');
+        }
+    });
+
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
+    });
+})(jQuery);
